319223023f8219bb7e2db79b864ce6c40af0064e
JivkoKelchev <jivko.r.kelchev@gmail.com>
2020-07-19 14:29:16 +0300

    hide mysql server command prompt

--- src/com/jivko/env_manager/Services/XammpControllerService.java
+++ src/com/jivko/env_manager/Services/XammpControllerService.java
@@ -1,7 +1,6 @@
 package com.jivko.env_manager.Services;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 
 public class XammpControllerService {
@@ -20,6 +19,8 @@ public class XammpControllerService {
             File hideScript = new File(getClass().getClassLoader().getResource("hide.vbs").getFile());
             String hideScriptPath = hideScript.getAbsolutePath();
             Process p = Runtime.getRuntime().exec("wscript.exe " + hideScriptPath + " " + dir + "/apache_start.bat");
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,7 +41,13 @@ public class XammpControllerService {
     public void startMySql(String dir) {
         try{
             System.out.println("start MySql on " + dir);
-            Process p = Runtime.getRuntime().exec("cmd /c start /B " + dir + "/mysql_start.bat");
+            //Process p = Runtime.getRuntime().exec("cmd /c start /B " + dir + "/mysql_start.bat");
+
+            File hideScript = new File(getClass().getClassLoader().getResource("hide.vbs").getFile());
+            String hideScriptPath = hideScript.getAbsolutePath();
+            Process p = Runtime.getRuntime().exec("wscript.exe " + hideScriptPath + " " + dir + "/mysql_start.bat");
+
+
         }catch (IOException e) {
             e.printStackTrace();
         }
